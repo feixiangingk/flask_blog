94c8a54994ad751fcf725d079bda0abcce616f95
feixiangingk <feixiangingk@163.com>
2018-04-15 15:20:50 +0800

    2018/4/15 小补丁

--- app/views.py
+++ app/views.py
@@ -3,9 +3,16 @@
 # createtime:   2018/4/14 13:59
 # software:     PyCharm
 # from models import db,User,Post,Comment
+from flask import jsonify
 
 def views_app(app):
 
     @app.route('/')
     def hello_world():
         return 'Hello World!'
+
+    @app.route('/test',methods=['POST','GET'])
+    def test():
+        return jsonify({'hello':'gufan'}),{'Access-Control-Allow-Origin':'127.0.0.1:8020',
+                                           "Access-Control-Allow-Headers": "X-Requested-With",
+                                           "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS"}
